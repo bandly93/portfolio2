d9dd9d3087a7612f66edb19efce06ddc851fb436
Band Ly <bandly93@gmail.com>
2019-07-29 22:05:15 -0700

    update portfolio

--- src/shared/components/Projects.js
+++ src/shared/components/Projects.js
@@ -2,39 +2,24 @@ import React , { Component,Fragment } from 'react';
 import { ProjectArray } from '../data/projectData';
 import '../styles/Project.css';
 
-class Projects extends Component{
-	
-	constructor(props){
-
-		super(props);
-
-		this.state = {
-			projectId : null,
-		}
-	}
-
-
-	setProjectId = (e) => {
-		console.log(e);
-	}
-
-	
+class Projects extends Component{	
 	mapProjects = () => {
 		return ProjectArray.map((project,i) => (
-			<div className = 'project-wrapper' key = {i}>	
+			<div className = 'project-wrapper' key = {i}>
 				<div className = 'project-text'>
 					<h1> {project.title} </h1>
 					<p className = 'grey-out med-font'> {project.subTitle} </p>
 					<p className = 'grey-out small-font'> {project.technologies} </p>
-					{
-						project.githubUrl?
-							<p> <a href = {project.githubUrl}> Github </a></p>
-						:
-							null
-					}
 				</div>
+				{
+					project.githubUrl?
+						<a title = "GitHub Link" href = {project.githubUrl}> <i className="fab fa-github-square"></i> </a>
+					:
+						null
+				}
+				<a title ="Website Link" href = {project.url}> <i className="fas fa-link"></i> </a>
 				<div className = 'project-img' >
-					<a href = {project.url}> <img src = {project.img} /></a>
+					<img src = {project.img} />
 				</div>
 				<hr/>	
 			</div>
